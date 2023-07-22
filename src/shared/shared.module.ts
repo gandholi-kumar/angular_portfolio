5b60e373f19226048f52c9fa9330e847a384477b
gandholi-kumar <46054848+gandholi-kumar@users.noreply.github.com>
2023-07-22 20:59:52 +0530

    4 files modified, 2 added

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -5,15 +5,23 @@ import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
 import { ContentComponent } from './content/content.component';
 import { NavComponent } from './nav/nav.component';
+import { TableContainerComponent } from './table-container/table-container.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule],
-  exports: [FooterComponent, HeaderComponent, NavComponent, ContentComponent],
+  exports: [
+    FooterComponent,
+    HeaderComponent,
+    NavComponent,
+    ContentComponent,
+    TableContainerComponent,
+  ],
   declarations: [
     FooterComponent,
     HeaderComponent,
     NavComponent,
     ContentComponent,
+    TableContainerComponent,
   ],
 })
 export class SharedModule {}
