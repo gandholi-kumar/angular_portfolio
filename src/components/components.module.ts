cde5dc4c96fe506494ea1b7bf6627a2dad6ab6f5
gandholi-kumar <46054848+gandholi-kumar@users.noreply.github.com>
2023-07-23 20:12:43 +0530

    4 files modified, 2 added

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -7,6 +7,7 @@ import { MedicalService } from './table-container/service/medical.service';
 import { SearchTextComponent } from './search-text/search-text.component';
 import { FormsModule } from '@angular/forms';
 import { TableFilterPipe } from './pipes/table-filter.pipe';
+import { HistoryGridComponent } from './history-grid/history-grid.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule],
@@ -15,14 +16,18 @@ import { TableFilterPipe } from './pipes/table-filter.pipe';
     HomeComponent,
     TableComponent,
     SearchTextComponent,
+    TableComponent,
     TableFilterPipe,
+    HistoryGridComponent
   ],
   exports: [
     TableContainerComponent,
     HomeComponent,
     TableComponent,
     SearchTextComponent,
+    TableComponent,
     TableFilterPipe,
+    HistoryGridComponent
   ],
   providers: [MedicalService],
 })
---
+++ src/components/history-grid/history-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-history-grid',
+  templateUrl: './history-grid.component.html',
+  styleUrls: ['./history-grid.component.css']
+})
+export class HistoryGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
